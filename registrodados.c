58fc7c590f0b3f00cbd4457517d50573905566b9
arthurd-rios <arthurd.rios10@gmail.com>
2025-04-13 11:59:17 -0300

    ADD: criar_dado

--- registrodados.c
+++ registrodados.c
@@ -1,5 +1,8 @@
 #include "registrodados.h"
 
+#define CAMPO_INT 0
+#define CAMPO_FLOAT 1
+
 struct registro_dado{
 
     int idAttack;
@@ -15,6 +18,122 @@ struct registro_dado{
 
 };
 
-bool criar_dado();
-bool escrever_dado();
-bool ler_dado();
+void criar_campoTamVar(FILE *fp, char *campo){
+
+    char tempchar;
+    int tamcampo = 0;
+
+    fread(&tempchar, sizeof(char), 1, fp);
+
+    while(tempchar != "," && tempchar != '\n'){
+
+        tamcampo++;
+        fread(&tempchar, sizeof(char), 1, fp);
+
+    }
+
+    if(tamcampo == 0){  // caso o campo seja vazio
+
+        campo = (char *)malloc(sizeof(char));
+        strncpy(campo, "0", 1);
+
+    }
+
+    campo = (char *)malloc((tamcampo + 1)*sizeof(char));
+
+    if(campo == NULL){
+
+        printf("Erro ao alocar espaço para o campo");
+        exit(1);
+
+    }
+
+    fseek(fp, -(tamcampo + 1), SEEK_CUR);
+    fread(campo, sizeof(char), tamcampo, fp);
+    campo[tamcampo] = '\0';
+
+    fseek(fp, 1, SEEK_CUR);
+
+}
+
+void criar_campoTamFixo(FILE *fp, void *campo, int tipo){
+
+    char tempchar;
+
+    fread(&tempchar, sizeof(char), 1, fp);
+
+    if(tipo == CAMPO_INT){
+
+        if(tempchar == ","){
+
+            *(int *)campo = -1;
+            return;
+
+        }
+
+        fseek(fp, -1, SEEK_CUR);
+        fread((int *)campo, sizeof(int), 1, fp);
+
+    }
+
+    if(tipo == CAMPO_FLOAT){
+
+        if(tempchar == ","){
+
+            *(float *)campo = -1.0;
+            return;
+
+        }
+
+        fseek(fp, -1, SEEK_CUR);
+        fread((float *)campo, sizeof(float), 1, fp);
+
+    }
+
+    fseek(fp, 1, SEEK_CUR);
+
+}
+
+dados *criar_dado(){
+
+    dados *regdados;
+
+    FILE *filein = fopen("nome.csv", "r");
+
+    regdados->removido = "0";
+    regdados->prox = -1;
+
+    if(filein == NULL){
+
+        printf("Falha ao abrir o arquivo");
+        exit(1);
+        
+    }
+
+    criar_campoTamFixo(filein, &(regdados->idAttack), CAMPO_INT); // ler idAttack
+    criar_campoTamFixo(filein, &(regdados->year), CAMPO_INT); // ler year
+    criar_campoTamFixo(filein, &(regdados->financialLoss), CAMPO_FLOAT); // ler financialLoss
+
+    criar_campoTamVar(filein, regdados->country); // ler country
+    criar_campoTamVar(filein, regdados->attackType); // ler attackType
+    criar_campoTamVar(filein, regdados->targetIndustry); // ler targetIndustry
+    criar_campoTamVar(filein, regdados->defenseMechanism); // ler defenseMechanism
+
+    regdados->tamanhoRegistro = strlen(regdados->country) + strlen(regdados->attackType) + strlen(regdados->targetIndustry) 
+    + strlen(regdados->defenseMechanism) + sizeof(regdados->idAttack) + sizeof(regdados->year) + sizeof(regdados->financialLoss);
+
+    fclose(filein);
+
+}
+
+bool escrever_dado(){
+
+
+
+}
+
+bool ler_dado(){
+
+
+
+}
--- registrodados.h
+++ registrodados.h
@@ -2,10 +2,11 @@
 #define _REG_DADO_H
 
     #include <stdbool.h>
+    #include <stdio.h>
 
     typedef struct registro_dado dados;
 
-    bool criar_cabecalho();
+    dados *criar_cabecalho();
     bool escrever_cabecalho();
     bool ler_cabecalho();
     // fazer um get para todos os campos do registro (?)
